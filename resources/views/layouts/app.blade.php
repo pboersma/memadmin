c8510d0f6a038f1bb45f384d28d2d8b77976cca9
Peter Boersma <peter@growingminds.nl>
2025-03-19 17:32:20 +0100

    Added Layout Base

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,19 +1,21 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html class="h-100" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>MemAdmin</title>
 
-    <!-- Styles / Scripts -->
     @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
         @vite(['resources/scss/app.scss', 'resources/js/app.js'])
     @endif
 </head>
+<body class="h-100 d-flex">
+    @include('partials.menu')
 
-<body>
+    <div class="container p-5">
+        @yield('content')
+    </div>
 </body>
-
 </html>
